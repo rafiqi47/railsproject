18566d073e347f56f62f263381ae010ae59cb62c
bilalahmed47 <bilal.ahmed@deltalabs.io>
2020-10-15 19:27:30 +0500

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,20 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])    
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+    if @user.update(user_params)
+      flash[:notice] = "Updated Successfuly!"
+      redirect_to articles_path
+    else
+      render 'eidt'
+    end
   end
 
   private 
