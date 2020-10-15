eb3ca99ab272654abc298dbc79d7b04019dce442
bilalahmed47 <bilal.ahmed@deltalabs.io>
2020-10-15 16:43:46 +0500

    association show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: {case_sensitive: false},
              length: {minimum: 3, maximum: 10}
-  VALID_EMAIL_REGEX = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/ 
-  validates :email, presence: true,uniqueness: {case_sensitive: false},
-             length: {maximun: 105}, format: {with: VALID_EMAIL_REGEX}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+             length: {minimum: 2, maximun: 105}, format: {with: VALID_EMAIL_REGEX}
 end
